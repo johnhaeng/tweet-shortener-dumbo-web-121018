003540f9965b8ff68ec42fe60a7d6fb17bbc66da
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-07 03:04:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,55 @@
 # Write your code here.
+
+def dictionary
+  new = {
+     "hello" => "hi", 
+     "to" => "2",
+     "two" => "2",
+     "too" => "2", 
+     "for" => "4",
+     "four" => "4", 
+     "be" => "b",
+     "you" => "u",
+     "at" => "@",
+     "and" => "&" 
+  }
+end
+
+
+def word_substituter(tweet)
+  tweet_words = tweet.split(' ')
+  new_tweet = []
+
+  tweet_words.map do |words|
+    dictionary.each do |word, short|
+      if words == word
+        words = short
+      end
+    end
+    new_tweet << words
+  end
+
+  new_tweet.join(' ')
+end
+
+
+def bulk_tweet_shortener(arr)
+  new_arr = []
+  
+  arr.each do |word|
+    if word_substituter(word) 
+      new_arr << word_substituter(word)
+    else 
+      new_arr << word
+    end
+  end
+  
+  puts new_arr  
+end
+
+
+
+
+
+
+
